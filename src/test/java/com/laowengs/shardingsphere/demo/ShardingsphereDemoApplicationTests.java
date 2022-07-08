61c4bc820904ace6291f5027be5ef286f0644320
wengjp <229595412@qq.com>
2022-07-08 17:51:12 +0800

    init

---
+++ src/test/java/com/laowengs/shardingsphere/demo/ShardingsphereDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laowengs.shardingsphere.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingsphereDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
